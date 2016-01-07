1557a5a8f7e07b1f1139bf57af1f011eaccd98e2
Lucas Baizer <titaniumsapphire32@gmail.com>
2016-01-06 17:50:47 -0800

    song

---
+++ src/karoke.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class karoke {
+	public static void main(String[] args) {
+		for (int i = 0; i < 2; i++) {
+
+			for (int n = 0; n < 3; n++) {
+				JOptionPane.showMessageDialog(null, "baby");
+			}
+			if (i == 0) {
+
+				JOptionPane.showMessageDialog(null, "oh");
+			} else {
+				JOptionPane.showMessageDialog(null, "no");
+			}
+
+		}
+		JOptionPane.showMessageDialog(null, "Thought you'd always be mine");
+	}
+}
