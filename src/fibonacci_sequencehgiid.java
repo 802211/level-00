e035e761b762aaa852213f5507bf3a06dbfba958
Mrfuzzy <league@WTS9.attlocal.net>
2016-02-17 18:31:17 -0800

    hide the creeper

---
+++ src/fibonacci_sequencehgiid.java
@@ -0,0 +1,28 @@
+public class fibonacci_sequencehgiid {
+public static void main(String[] args) {
+	
+	
+int	number1 = 1;
+int	number2 = 0;
+
+	
+for (int i = 0; i < 10; i++) {
+	
+	System.out.println(number2);
+	number2 = number1 +  number2;
+	System.out.println(number1);
+	number1 = number1 + number2;
+
+
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
